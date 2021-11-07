fd760d4811cdbfa00ed99bd070a3d0288785e19c
Aaronrex <alvaroandres227@hotmail.com>
2021-11-06 21:46:07 -0500

    create pagination

--- src/app/components/hero-list/hero-list.component.ts
+++ src/app/components/hero-list/hero-list.component.ts
@@ -9,8 +9,11 @@ import { HeroserviceService } from 'src/services/heroservice.service';
 })
 export class HeroListComponent implements OnInit {
   heroes? : RootObject;
+  pagesArray: Number[] = [1,2,3,4,5];
   subscription?: Subscription;
-  constructor(private heroService :   HeroserviceService) { }
+  actualPage: Number = 0;
+  constructor(private heroService :   HeroserviceService) {
+   }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -22,6 +25,15 @@ export class HeroListComponent implements OnInit {
   }
   ngOnDestroy() {
     this.subscription?.unsubscribe();
+  }
+  getPaginatedHeroes(pageNumber: number):void{
+    this.actualPage = pageNumber;
+    this.subscription = this.heroService.getCharacter(10*pageNumber).subscribe((heroes) =>{
+      this.heroes = heroes;
+    })
+  }
+  getActualPage():Number{
+    return this.actualPage;
   }
 
 }
